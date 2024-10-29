1f524964732d2a13f0b107165744b85d22eff42f
basicfuton <macmatt2003@gmail.com>
2024-10-29 15:01:24 -0700

    Add project files.

---
+++ README.md
@@ -0,0 +1,2 @@
+# FutonsAdditions
+Replace with your mod's readme!
---
+++ Source/FutonsAdditionsModule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Celeste.Mod.FutonsAdditions;
+
+public class FutonsAdditionsModule : EverestModule {
+    public static FutonsAdditionsModule Instance { get; private set; }
+
+    public override Type SettingsType => typeof(FutonsAdditionsModuleSettings);
+    public static FutonsAdditionsModuleSettings Settings => (FutonsAdditionsModuleSettings) Instance._Settings;
+
+    public override Type SessionType => typeof(FutonsAdditionsModuleSession);
+    public static FutonsAdditionsModuleSession Session => (FutonsAdditionsModuleSession) Instance._Session;
+
+    public override Type SaveDataType => typeof(FutonsAdditionsModuleSaveData);
+    public static FutonsAdditionsModuleSaveData SaveData => (FutonsAdditionsModuleSaveData) Instance._SaveData;
+
+    public FutonsAdditionsModule() {
+        Instance = this;
+#if DEBUG
+        // debug builds use verbose logging
+        Logger.SetLogLevel(nameof(FutonsAdditionsModule), LogLevel.Verbose);
+#else
+        // release builds use info logging to reduce spam in log files
+        Logger.SetLogLevel(nameof(FutonsAdditionsModule), LogLevel.Info);
+#endif
+    }
+
+    public override void Load() {
+        // TODO: apply any hooks that should always be active
+    }
+
+    public override void Unload() {
+        // TODO: unapply any hooks applied in Load()
+    }
+}
